d9586262fd2dd0fd4a78e45968d6b912afe4d85b
sergio-asenjo <20078140+sergio-asenjo@users.noreply.github.com>
2022-09-02 00:13:46 -0400

    chore: adds api call and displays data.

--- apps/monsters-rolodex/src/app/app.tsx
+++ apps/monsters-rolodex/src/app/app.tsx
@@ -1,14 +1,66 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import React from 'react';
+import './app.scss';
 
-export function App() {
-  return (
-    <>
-      <NxWelcome title="monsters-rolodex" />
-      <div />
-    </>
-  );
+interface IState {
+  monsters: IMonster[];
+}
+
+interface IMonster {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  override async componentDidMount(): Promise<void> {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+    this.setState(() => {
+      return { monsters: users };
+    });
+  }
+
+  override render() {
+    return (
+      <div className="app">
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
