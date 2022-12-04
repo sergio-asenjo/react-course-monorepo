88585060c49eb1ad20bc44e077dcd4e818ba3d2f
sergio-asenjo <20078140+sergio-asenjo@users.noreply.github.com>
2022-12-03 23:40:41 -0300

    feat: adds search input to filter monsters.

--- apps/monsters-rolodex/src/app/app.tsx
+++ apps/monsters-rolodex/src/app/app.tsx
@@ -36,6 +36,8 @@ interface Company {
 }
 
 export class App extends React.Component<{}, IState> {
+  private originalMonsters: IMonster[] = [];
+
   constructor(props: {}) {
     super(props);
 
@@ -44,22 +46,38 @@ export class App extends React.Component<{}, IState> {
     };
   }
 
+  override render() {
+    return (
+      <div className="app">
+        <input
+          className="search-input"
+          type="search"
+          placeholder="Search monsters"
+          onChange={this.handleChange}
+        />
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
+
   override async componentDidMount(): Promise<void> {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const users = await response.json();
     this.setState(() => {
       return { monsters: users };
     });
+    this.originalMonsters = users;
   }
 
-  override render() {
-    return (
-      <div className="app">
-        {this.state.monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
-        })}
-      </div>
+  private handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const filteredMonsters = this.originalMonsters.filter((monster) =>
+      monster.name.toLowerCase().includes(e.target.value.toLowerCase())
     );
+    this.setState(() => {
+      return { monsters: filteredMonsters };
+    });
   }
 }
 
