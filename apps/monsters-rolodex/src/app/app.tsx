e33dae730cb0011fbf299d8dfa8820d5adfc29f3
sergio-asenjo <20078140+sergio-asenjo@users.noreply.github.com>
2022-12-05 23:33:43 -0300

    chore: moves app component to functional component.

--- apps/monsters-rolodex/src/app/app.tsx
+++ apps/monsters-rolodex/src/app/app.tsx
@@ -1,14 +1,47 @@
 import Card from '@components/card/card.component';
 import SearchBox from '@components/search-box/search-box.component';
 import { IMonster } from '@interfaces/IMonster';
-import React from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import './app.scss';
 
 interface IState {
   monsters: IMonster[];
 }
 
-export class App extends React.Component<{}, IState> {
+const App = () => {
+  const [monsters, setMonsters] = useState<IMonster[]>([]);
+  const [originalMonsters, setOriginalMonsters] = useState<IMonster[]>([]);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const filteredMonsters = originalMonsters.filter((monster) =>
+      monster.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setMonsters(filteredMonsters);
+  };
+  
+  useEffect(() => {
+    const fetchMonsters = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const users = await response.json();
+      setMonsters(users);
+      setOriginalMonsters(users);
+    };
+    fetchMonsters();
+  }, []);
+  
+  return (
+    <div className="app">
+      <h1 className="title">Monsters Rolodex</h1>
+      <SearchBox placeholder="Search monsters" onChangeHandler={handleChange} />
+      <div className="monsters__container">
+        {monsters.map((monster) => {
+          return <Card monster={monster} key={monster.id}/>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+/* export class App extends React.Component<{}, IState> {
   private originalMonsters: IMonster[] = [];
 
   constructor(props: {}) {
@@ -53,6 +86,6 @@ export class App extends React.Component<{}, IState> {
       return { monsters: filteredMonsters };
     });
   }
-}
+} */
 
 export default App;
