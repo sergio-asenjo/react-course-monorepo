302556dedef734963e0e7fecdd2ef7f0e08afa8e
sergio-asenjo <20078140+sergio-asenjo@users.noreply.github.com>
2023-01-10 23:15:02 -0300

    chore: add correct styling for checkout.

--- apps/cap-clothing/src/app/components/checkout-item/checkout-item.component.tsx
+++ apps/cap-clothing/src/app/components/checkout-item/checkout-item.component.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from 'apps/cap-clothing/src/utils/number.utils';
 import { ICartItem } from '../../interfaces/ICartItem';
 
 import './checkout-item.styles.scss';
@@ -18,8 +19,8 @@ export const CheckoutItem = ({
   const { name, imageUrl, price, quantity } = cartItem;
 
   return (
-    <div className="co-item flex-row">
-      <img src={imageUrl} alt="item" />
+    <div className="co-item">
+      <img src={imageUrl} alt="item" className="co-item__img" />
       <div className="co-item__name">{name}</div>
       <div className="co-item__quantity">
         <span className="symbol" onClick={() => decreaseQuantity(cartItem)}>
@@ -30,7 +31,7 @@ export const CheckoutItem = ({
           &gt;
         </span>
       </div>
-      <div className="co-item__price">{price}</div>
+      <div className="co-item__price">{formatCurrency(price*quantity!)}</div>
       <div className="co-item__remove" onClick={() => removeItem(cartItem)}>
         &#10005;
       </div>
--- apps/cap-clothing/src/app/routes/checkout/checkout.component.tsx
+++ apps/cap-clothing/src/app/routes/checkout/checkout.component.tsx
@@ -6,37 +6,32 @@ import { CartContext } from '../../context/cart.context';
 import './checkout.styles.scss';
 
 export const Checkout = () => {
-  const { cartItems, addItemToCart, reduceItemFromCart, removeItemFromCart } = useContext(CartContext);
+  const { cartItems, addItemToCart, reduceItemFromCart, removeItemFromCart } =
+    useContext(CartContext);
 
   return (
     <div className="checkout-page">
-      <div className="checkout-header flex-row">
-        <div className="header-block">
-          Product
-        </div>
-        <div className="header-block">
-          Description
-        </div>
-        <div className="header-block">
-          Quantity
-        </div>
-        <div className="header-block">
-          Price
-        </div>
-        <div className="header-block">
-          Remove
-        </div>
+      <div className="checkout-header">
+        <div className="header-block">Product</div>
+        <div className="header-block">Description</div>
+        <div className="header-block">Quantity</div>
+        <div className="header-block">Price</div>
+        <div className="header-block">Remove</div>
       </div>
       <div className="flex-col">
-        {cartItems.map((cartItem) => (
-          <CheckoutItem 
-            key={cartItem.id}
-            cartItem={cartItem}
-            removeItem={removeItemFromCart}
-            increaseQuantity={addItemToCart}
-            decreaseQuantity={reduceItemFromCart}
-          />
-        ))}
+        {cartItems.length === 0 ? (
+          <span className="empty-message">Your cart is empty!</span>
+        ) : (
+          cartItems.map((cartItem) => (
+            <CheckoutItem
+              key={cartItem.id}
+              cartItem={cartItem}
+              removeItem={removeItemFromCart}
+              increaseQuantity={addItemToCart}
+              decreaseQuantity={reduceItemFromCart}
+            />
+          ))
+        )}
       </div>
     </div>
   );
