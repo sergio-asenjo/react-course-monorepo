4acefb19441a06d9a0a42dfb3b938569a44caf66
sergio-asenjo <20078140+sergio-asenjo@users.noreply.github.com>
2023-03-24 23:39:53 -0300

    feat: add notes api.

---
+++ apps/notes-manager/src/app/api/notes.ts
@@ -0,0 +1,61 @@
+const BASE_API = 'http://localhost:3000/notes';
+
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+}
+
+export class NotesService {
+
+  public static async fetchAll(): Promise<Note[]> {
+    try {
+      const response = await fetch(`${BASE_API}`);
+      const notes = await response.json();
+      return notes;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  public static async fetchById(id: number): Promise<Note | null> {
+    try {
+      const response = await fetch(`${BASE_API}/${id}`);
+      const note = await response.json();
+      return note;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
+  public static async create(note: Note): Promise<boolean> {
+    try {
+      await fetch(`${BASE_API}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  public static async deleteById(id: number): Promise<boolean> {
+    try {
+      await fetch(`${BASE_API}/${id}`, {
+        method: 'DELETE'
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+}
